88861cbdb22d97b5d67a04a3a032c4d11df06015
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-10-09 21:54:07 +0800

    Handled missing zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -14,8 +14,9 @@ contents.each do |row|
     # If the zip code is exactly five digits, assume that it is ok
     # If the zip code is more than five digits, truncate it to the first five digits
     # If the zip code is less than five digits, add zeros to the front until it becomes five digits
-    
-    if zipcode.length < 5
+    if zipcode.nil?
+        zipcode = '00000'
+    elsif zipcode.length < 5
         zipcode = zipcode.rjust(5, '0')
     elsif zipcode.length > 5
         zipcode = zipcode[0..4]
