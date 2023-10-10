c2e656d4be18e1dd6eaf3e243abf5c03a7aa9fc2
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-10-10 14:47:02 +0800

    Showed all legislators in a zip code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,13 @@
 require 'csv'
 
+require 'google/apis/civicinfo_v2'
+
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+
+
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -18,6 +26,18 @@ contents.each do |row|
 
     zipcode = clean_zipcode(row[:zipcode])
 
+    begin
+        legislators = civic_info.representative_info_by_address(
+        address: zipcode,
+        levels: 'country', 
+        roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+        legislators = legislators.officials
+    rescue
+        'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
+    
+    
 
     puts "#{name} #{zipcode}"
 end
